02157ea6ba06e4887696db0526de82a276465326
ramjisreenivasan <63060215+ramjisreenivasan@users.noreply.github.com>
2025-07-24 18:18:58 -0400

    included initial ionic updates for android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hourbank.app',
+  appName: 'HourBank',
+  webDir: 'www'
+};
+
+export default config;
